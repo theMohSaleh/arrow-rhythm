0c4954b5d17ce99f542ebe56c5566cdb414c7f3f
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-08 11:02:18 +0300

    drawing arrow on screen and check for beat timing done

--- js/app.js
+++ js/app.js
@@ -1,42 +1,29 @@
 /*-------------- Constants -------------*/
 
-const arrowTypesArray = [
-    "↑",
-    "→",
-    "↓",
-    "←",
-];
-
 const levelCombos = [
-    // ["→", "→", "↑", "↓"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
-    // ["↑", "←", "↑", "↑"],
     [
-        { arrow: "→", pos: { x: 100, y: 50 } },
-        { arrow: "→", pos: { x: 120, y: 50 } },
-        { arrow: "↑", pos: { x: 140, y: 50 } },
-        { arrow: "↓", pos: { x: 160, y: 50 } },
+        { arrow: "→", startPos: { x: 100, y: 50 }, hitPos: { x: 210, y: 160 }, velocity: 1 },
+        { arrow: "→", startPos: { x: 140, y: 50 }, hitPos: { x: 250, y: 160 }, velocity: 1 },
+        { arrow: "↑", startPos: { x: 180, y: 50 }, hitPos: { x: 290, y: 160 }, velocity: 1 },
+        { arrow: "↓", startPos: { x: 210, y: 50 }, hitPos: { x: 320, y: 160 }, velocity: 1 },
     ],
     [
-        { arrow: "↑", pos: { x: 140, y: -100 } },
-        { arrow: "→", pos: { x: 100, y: -100 } },
-        { arrow: "↓", pos: { x: 160, y: -100 } },
-        { arrow: "→", pos: { x: 120, y: -100 } },
+        { arrow: "→", startPos: { x: 210, y: 180 }, hitPos: { x: 100, y: 70 }, velocity: -1 },
+        { arrow: "→", startPos: { x: 250, y: 180 }, hitPos: { x: 140, y: 70 }, velocity: -1 },
+        { arrow: "↑", startPos: { x: 290, y: 180 }, hitPos: { x: 180, y: 70 }, velocity: -1 },
+        { arrow: "↓", startPos: { x: 320, y: 180 }, hitPos: { x: 210, y: 70 }, velocity: -1 },
     ],
     [
-        { arrow: "↑", pos: { x: 140, y: 20 } },
-        { arrow: "→", pos: { x: 100, y: 20 } },
-        { arrow: "↓", pos: { x: 160, y: 20 } },
-        { arrow: "→", pos: { x: 120, y: 20 } },
+        { arrow: "↑", startPos: { x: 140, y: 90 }, hitPos: { x: 210, y: 150 }, velocity: 1 },
+        { arrow: "→", startPos: { x: 100, y: 90 }, hitPos: { x: 200, y: 150 }, velocity: 1 },
+        { arrow: "↓", startPos: { x: 160, y: 90 }, hitPos: { x: 260, y: 150 }, velocity: 1 },
+        { arrow: "→", startPos: { x: 120, y: 90 }, hitPos: { x: 220, y: 150 }, velocity: 1 },
+    ],
+    [
+        { arrow: "↑", startPos: { x: 140, y: 20 }, hitPos: { x: 250, y: 130 }, velocity: 1 },
+        { arrow: "→", startPos: { x: 100, y: 20 }, hitPos: { x: 210, y: 130 }, velocity: 1 },
+        { arrow: "↓", startPos: { x: 160, y: 20 }, hitPos: { x: 270, y: 130 }, velocity: 1 },
+        { arrow: "→", startPos: { x: 120, y: 20 }, hitPos: { x: 230, y: 130 }, velocity: 1 },
     ],
 ]
 
@@ -55,9 +42,10 @@ let hp;
 let arrCount;
 let diffIndex;
 let lose;
-let velocity;
+let arrowSpeed;
 let game;
-
+let incrementInterval;
+let incrementTimeout;
 
 /*----- Cached Element References  -----*/
 
@@ -75,55 +63,38 @@ const ctx = canvas.getContext("2d");
 
 // arrow class that represents each arrow drawn on the screen
 class Arrow {
-    constructor(x, y, dx, dy) {
+    constructor(arrowType, x, y, hitX, hitY, velocity) {
         this.x = x; // X axis on the canvas
         this.y = y; // Y axis on the canvas
-        this.dx = dx; // X velocity
-        this.dy = dy; // Y velocity
+        this.dx = velocity; // X velocity
+        this.dy = velocity; // Y velocity
         this.startTimer = performance.now();
-        this.arrowDuration = 3000;
+        this.arrowDuration = 1900;
         this.stopped = false;
         this.missed = false;
-        this.arrowType = arrowTypesArray[Math.floor(Math.random() * 4)]
+        this.arrowType = arrowType;
 
         this.draw = function () {
             ctx.font = "30px Arial"
             ctx.fillText(this.arrowType, this.x, this.y);
 
-            ctx.strokeText(this.arrowType, canvas.width / 2 + 50, canvas.height / 2 + 100)
-            // if (x + 40 > canvas.width - 40) {
-            //     ctx.strokeText(this.arrowType, x - 40, y - 40)
-            // } else {
-            //     ctx.strokeText(this.arrowType, x + 40, y + 40)
-            // }
+            ctx.strokeText(this.arrowType, hitX, hitY);
         }
 
         this.checkBeat = function () {
             const time = performance.now();
-            const arrowTime = time - this.startTimer;
-                        
-            if (Math.abs(arrowTime - this.arrowDuration) <= this.startTimer) {
+            const arrowTime = time - this.startTimer;            
+            
+            if (Math.abs(arrowTime - this.arrowDuration) < 200 && Math.abs(arrowTime - this.arrowDuration) > -100) {
+                console.log(Math.abs(arrowTime));
                 console.log("arrow hit");
-                return;
-            } else {
-                console.log('missed');
                 return;
             }
         }
 
         this.update = function () {
             if (this.stopped) {
-                console.log('arrow stopped');
                 return;
-            }
-
-            if (this.x > canvas.width - 40 || this.x - 1 < 0) {
-                this.dx = -this.dx;
-            }
-
-            // avoid hp bar
-            if (this.y + 96 > canvas.height * 1.3 || this.y - canvas.height / 20 < 50) {
-                this.dy = -this.dy;
             }
 
             this.x += this.dx;
@@ -133,7 +104,7 @@ class Arrow {
             setTimeout(() => {
                 this.stopped = true;
                 return
-            }, 3100);
+            }, this.arrowDuration);
 
             this.draw();
         }
@@ -149,16 +120,18 @@ function init() {
     }
     lose = false;
     retryBtn.classList.add('hidden');
-    velocity = 0;
+    arrowSpeed = 0;
 }
 
 function render() {
     diffText.textContent = gameDifficulty[diffIndex];
 }
 
+// initalize on load
 init();
 render();
 
+// function to start the game
 function startGame() {
     playBtn.classList.add('hidden');
     diffSection.classList.add('hidden');
@@ -169,31 +142,30 @@ function startGame() {
 
     switch (gameDifficulty[diffIndex]) {
         case 'Easy':
-            velocity = 1;
+            arrowSpeed = 1;
             break;
         case 'Normal':
-            velocity = 2;
+            arrowSpeed = 2;
             break;
         case 'Hard':
-            velocity = 3;
+            arrowSpeed = 3;
             break;
     }
-
-    game = setInterval(() => {
-        arrCount++;
-    }, 1000);
     drawGame();
-
     renderArrows(0);
 }
 
+// function to stop game and display a game over screen
 function gameOver() {
     diffSection.classList.remove('hidden');
     retryBtn.classList.remove('hidden');
     lose = true;
     clearInterval(game);
+    clearInterval(incrementInterval);
+    clearTimeout(incrementTimeout);
 }
 
+// function to draw and update the canvas
 function drawGame() {
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -209,10 +181,13 @@ function drawGame() {
 
     ctx.restore();
 
-    //render arrow
+    //render arrows
+    if (arrowArray.length) {
+        updateArrows();
+    }
 
     // if hp is at 0, stop call gameover and stop drawing
-    if (hp <= 0) {
+    if (hp < 1) {
         gameOver();
         return;
     }
@@ -221,46 +196,69 @@ function drawGame() {
     requestAnimationFrame(drawGame)
 }
 
-function spawnArrow() {
-    arrowArray[arrCount].update();
-    // arrowArray[arrCount + 1].update();
-    // arrowArray[arrCount + 2].update();
+// function to call update each animation for each arrow 
+function updateArrows() {
+    // spawn 3 arrows at max each time
+    let arrow1 = arrowArray[arrCount];
+    let arrow2 = arrowArray[arrCount + 1];
+    let arrow3 = arrowArray[arrCount + 2];
+    // only call update if arrow exists
+    if (arrow1) {
+        arrowArray[arrCount].update();
+    }
+    if (arrow2) {
+        arrowArray[arrCount + 1].update();
+    }
+    if (arrow3) {
+        arrowArray[arrCount + 2].update();
+    }
 }
 
 function renderArrows(comboIdx) {
-    console.log(levelCombos[comboIdx]);
-    
+    // get the set combo in the level combos array
     const combo = levelCombos[comboIdx];
-    let delay = 0;
+    let delay = 0 // delay to the arrows to spawn
 
+    incrementTimeout = setTimeout(() => {
+        incrementInterval = setInterval(() => {
+            arrCount++;
+        }, 1000);
+    }, 1900);
+
+    // wait 1 second and then spawn next arrow
     combo.forEach((arr, index) => {
         setTimeout(() => {
             drawArrow(arr);
         }, delay);
-
         delay += 1000;
     });
 
+    clearInterval(incrementInterval);
+
+    // after the previous foreach ends, wait 2 seconds and then reinvoke this function to start next combo
     setTimeout(() => {
-        // start next combo
-        if (comboIdx < levelCombos.length - 1) {
-            renderArrows(comboIdx+1)
+        // if there are more combos, start next combo
+        if (comboIdx < levelCombos.length - 1 && !lose) {
+            renderArrows(comboIdx + 1)
         }
     }, delay + 2000);
 }
 
 function drawArrow(arr) {
-    ctx.font = "30px Arial";
-    ctx.fillStyle = 'black';
-    ctx.fillText = (arr.arrow, arr.pos.x, arr.pos.y);
-
+    arrowArray.push(new Arrow(
+        arr.arrow,
+        arr.startPos.x,
+        arr.startPos.y,
+        arr.hitPos.x,
+        arr.hitPos.y,
+        arr.velocity * arrowSpeed,
+    ));
 }
 
 
 // function to handle difficulty change click
 function handleDiffClick(event) {
     // check which button was clicked on
-
     if (event.target.id === 'nextBtn') {
         // check if user is at the end of the array, reset index value to the start if true
         if (diffIndex + 1 >= gameDifficulty.length) {
@@ -277,7 +275,7 @@ function handleDiffClick(event) {
         }
     }
 
-    // show new difficulty
+    // display new difficulty text
     diffText.textContent = gameDifficulty[diffIndex];
 }
 
@@ -285,12 +283,10 @@ function handlePlayClick(event) {
     startGame();
 }
 
-
 function handleRetryClick() {
     init();
     startGame();
 }
-
 
 function handleKeyPress(event) {
     if (event.code === 'Space') {
@@ -301,7 +297,6 @@ function handleKeyPress(event) {
         })
     }
 }
-
 
 /*----------- Event Listeners ----------*/
 
